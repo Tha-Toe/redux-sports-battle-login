430f215eb5d853b32bcde249a7945922ea4b15c7
tha toe saung <thatoe510@gmail.com>
2022-10-12 15:27:57 +0630

    add redirect file for netlify

--- src/protected/Redirect.js
+++ src/protected/Redirect.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
+import LoadingSpinner from "../component/loadingSpinner/LoadingSpinner";
 export default function Redirect({ children }) {
-  const { user } = UserAuth();
-  if (user) {
+  const { user, checking } = UserAuth();
+  if (checking) {
+    return <LoadingSpinner />;
+  } else if (user) {
     return <Navigate to="/home" />;
   }
   return children;
