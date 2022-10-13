d354fa114e40674f8e3cea592fb2d2bc2c27e629
varunvasista <varunvashista@engineer.com>
2022-10-13 15:26:13 +0530

    update prop

--- src/context/PropsContext.js
+++ src/context/PropsContext.js
@@ -1,6 +1,7 @@
 import { useContext, createContext, useEffect, useState } from "react";
 import { UserAuth } from "./AuthContext";
-
+import { APIURLs } from "../api/ApiUrls";
+import { makeGETAPICall } from "../api/methods";
 const PropsContext = createContext();
 
 export const PropsContextProvider = ({ children }) => {
@@ -21,4 +22,14 @@ export const PropsContextProvider = ({ children }) => {
 
 export const PropsData = () => {
   return useContext(PropsContext);
+};
+
+export const getAllSports = async () => {
+  var apiUrl = APIURLs.getAllSports;
+  const apiResponse = await makeGETAPICall(apiUrl);
+  if (apiResponse.status === 200) {
+    return apiResponse.data;
+  } else {
+    return null;
+  }
 };
