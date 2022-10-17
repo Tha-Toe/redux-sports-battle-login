252b701c11eef44cef8d45aeadecafc6d333e32d
varunvasista <varunvashista@engineer.com>
2022-10-17 15:00:42 +0530

    firebase update

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -33,5 +35,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const analytics = getAnalytics(app);
+const storage = getStorage(app);
+const db = getFirestore(app);
 
-export { auth, analytics };
+export { auth, analytics, storage, db };
