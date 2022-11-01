05cc8217a792d634dcfd1af052ceac478338bb38
varunvasista <varunvashista@engineer.com>
2022-11-01 18:08:11 +0530

    prop cart method update

--- src/component/Props/PropCart.jsx
+++ src/component/Props/PropCart.jsx
@@ -2,6 +2,7 @@ import { Box, Typography } from "@mui/material";
 import React from "react";
 import "./props.css";
 import { useSelector } from "react-redux";
+import _ from 'lodash';
 
 export default function PropCart({ selectedCardList, mode }) {
   const fs = useSelector((state) => state.user.fs);
@@ -132,3 +133,125 @@ export default function PropCart({ selectedCardList, mode }) {
     </Box>
   );
 }
+
+export const getPayouts = (selectedUserProps, metadata) => {
+  if (!selectedUserProps || selectedUserProps.length < 2) {
+    return {};
+  }
+  const statsInThisEntry = _.uniq(_.map(selectedUserProps, o => o.statKey + '::' + o.sport));
+  const sportsInThisEntry = _.uniq(_.map(selectedUserProps, o => o.sport));
+  const uniqueCorrSportsInThisEntry = _.uniq(_.map(selectedUserProps.filter(x => metadata.correlatedSports.includes(x.sport)), o => o.sport));
+  const attackOnlySportsInThisEntry = _.uniq(_.map(selectedUserProps.filter(x => metadata.attackOnlySports.includes(x.sport)), o => o.sport));
+
+  let appPayouts = metadata.payouts;
+  let payoutsHere = JSON.parse(JSON.stringify(appPayouts));
+
+  const numPicks = selectedUserProps.length;
+  const attackPayouts = payoutsHere.filter(x => x.totalPicks === numPicks)[0]['attack'];
+  const attackPayoutsForStatsInThisEntry = attackPayouts.filter(x => statsInThisEntry.length === x.numStats)[0];
+  const defensePayouts = payoutsHere.filter(x => x.totalPicks === numPicks)[0]['defense'];
+  const defensePayoutsForStatsInThisEntry = defensePayouts.filter(x => statsInThisEntry.length === x.numStats);
+
+  let totalPayoutInThisEntry = 0.0;
+  const correlationSportEntries = selectedUserProps.filter(x => metadata.correlatedSports.includes(x.sport));
+  let numOfCorrelatedSportEntries = correlationSportEntries.length;
+
+  if (numOfCorrelatedSportEntries > 0) {
+    // let payoutContributedByCorrSports = (numOfCorrelatedSportEntries / selectedUserProps.length) * attackPayoutsForStatsInThisEntry.payout * metadata.correlatedPayout;
+    // let payoutContributedByCorrSports = (numOfCorrelatedSportEntries / selectedUserProps.length) * attackPayoutsForStatsInThisEntry.payout;
+
+    let payoutContributedByCorrSports = (numOfCorrelatedSportEntries / selectedUserProps.length) * attackPayoutsForStatsInThisEntry.payout;
+    const payoutContributedByOtherSports = ((selectedUserProps.length - numOfCorrelatedSportEntries) / selectedUserProps.length) * attackPayoutsForStatsInThisEntry.payout;
+
+    const considerOtherSportsForPayoutReduction = payoutContributedByOtherSports > 0 && sportsInThisEntry.length - uniqueCorrSportsInThisEntry.length >= 2;
+    const action = _.map(correlationSportEntries, o => o.action === appConstants.overButtonAction);
+    const numOvers = _.filter(correlationSportEntries, o => o.action === appConstants.overButtonAction).length;
+    const numUnders = _.filter(correlationSportEntries, o => o.action === appConstants.underButtonAction).length;
+
+    let sameStatAllSameActionReduction = false;
+    if (numOfCorrelatedSportEntries >= 4 || considerOtherSportsForPayoutReduction) {
+      let distinctCorrelatedStats = _.uniq(_.map(correlationSportEntries, o => o.statKey));
+      distinctCorrelatedStats.forEach(x => {
+        if (!sameStatAllSameActionReduction) {
+          const thisStatPicks = _.filter(correlationSportEntries, o => o.statKey === x);
+          const thisStatActions = _.uniq(_.map(thisStatPicks, o => o.action));
+          if (thisStatPicks.length > 1) {
+            if (thisStatActions.length === 1) {
+              sameStatAllSameActionReduction = true;
+            }
+          } else {
+            if (x === 'gir' || x === 'fairwaysHit' || x === 'birdiesOrBetter') {
+              if (thisStatActions[0] === appConstants.underButtonAction) {
+                sameStatAllSameActionReduction = true;
+              }
+            } else if (x === 'strokes') {
+              if (thisStatActions[0] === appConstants.overButtonAction) {
+                sameStatAllSameActionReduction = true;
+              }
+            }
+          }
+        }
+      });
+    }
+
+    let correlationPayoutPerc = 1.0;
+    if (numOfCorrelatedSportEntries === 2) {
+      if (numOvers === 0 || numUnders === 0) {
+        correlationPayoutPerc = 0.865;
+      } else {
+        correlationPayoutPerc = 1.0;
+      }
+    } else if (numOfCorrelatedSportEntries === 3) {
+      if (numOvers === 0 || numUnders === 0) {
+        correlationPayoutPerc = 0.75;
+      } else {
+        correlationPayoutPerc = 1;
+      }
+    } else if (numOfCorrelatedSportEntries >= 4) {
+      if (numOvers === 0 || numUnders === 0) {
+        correlationPayoutPerc = 0.5;
+      } else {
+        let biggerChoice;
+        if (numOvers > numUnders) {
+          biggerChoice = numOvers;
+        } else {
+          biggerChoice = numUnders;
+        }
+        let biggerChoicePerc = 100 - (biggerChoice / (numOvers + numUnders)) * 100;
+        if (biggerChoicePerc <= 25) {
+          correlationPayoutPerc = 0.65;
+        } else {
+          correlationPayoutPerc = 0.8;
+        }
+      }
+    }
+    let finalPayoutContributedByCorrSports = payoutContributedByCorrSports * correlationPayoutPerc;
+    totalPayoutInThisEntry = finalPayoutContributedByCorrSports + payoutContributedByOtherSports;
+    if (sameStatAllSameActionReduction || considerOtherSportsForPayoutReduction) {
+      totalPayoutInThisEntry = totalPayoutInThisEntry * 1;
+    }
+    totalPayoutInThisEntry = totalPayoutInThisEntry >= 3 ? Math.round(totalPayoutInThisEntry * 2) / 2 : totalPayoutInThisEntry;
+    if (totalPayoutInThisEntry === 5.5) {
+      totalPayoutInThisEntry = 5;
+    } else if (totalPayoutInThisEntry >= 2.59 && totalPayoutInThisEntry < 2.6) {
+      totalPayoutInThisEntry = 3;
+    } else if (totalPayoutInThisEntry > 4 && totalPayoutInThisEntry <= 5) {
+      totalPayoutInThisEntry = 5;
+    } else if (totalPayoutInThisEntry > 10 && totalPayoutInThisEntry <= 11) {
+      totalPayoutInThisEntry = 10;
+    } else if (numOfCorrelatedSportEntries >= 2 && totalPayoutInThisEntry > 13) {
+      totalPayoutInThisEntry = 13.0;
+    }
+  } else {
+    totalPayoutInThisEntry = attackPayoutsForStatsInThisEntry.payout;
+  }
+  totalPayoutInThisEntry = parseFloat(totalPayoutInThisEntry.toFixed(2));
+  attackPayoutsForStatsInThisEntry.payout = totalPayoutInThisEntry;
+
+  const payoutsForThisEntry = {
+    attackPayouts: [attackPayoutsForStatsInThisEntry],
+    defensePayouts: defensePayoutsForStatsInThisEntry,
+    attackOnlyEntry: attackOnlySportsInThisEntry.length > 0 || selectedUserProps.length <= 2,
+  };
+  return payoutsForThisEntry;
+};
