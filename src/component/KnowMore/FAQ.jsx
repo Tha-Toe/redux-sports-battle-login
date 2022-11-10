415018da051e724dc167830030f49d12219ec518
tha toe saung <thatoe510@gmail.com>
2022-11-10 13:38:23 +0630

    add faq

--- src/component/KnowMore/FAQ.jsx
+++ src/component/KnowMore/FAQ.jsx
@@ -1,108 +1,152 @@
 import { Box, Typography } from "@mui/material";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import "./knowMore.css";
 import { APIURLs } from "../../api/ApiUrls";
 import { makeGETAPICall } from "../../api/methods";
+import LoadingSpinnerEachSection from "../loadingSpinner/LoadingSpinnerEachSection";
 
-export default function FAQ() {
+export default function FAQ({ openTag }) {
   const fs = useSelector((state) => state.user.fs);
-  const [FAQnote, setFAQnote] = useState([
-    {
-      head: "What is SportsBattle?",
-      paragraph:
-        "ASportsBattle is a skill-based Daily Fantasy Sports destination for fans in United States of America to compete in single-day online contests across the largest variety of leagues.",
-    },
-    {
-      head: "Is Fantasy Sports legal?",
-      paragraph:
-        "Ofcourse it is. Fantasy is a game of skill and not gambling which requires the players possess immense knowledge of the game, players and be upto date with the playing 11 and team squads. However, playing cash contests is not legal in certain states in United States of America. You can read about it below. ",
-    },
-    {
-      head: "Is Playing on SportsBattle legal?",
-      paragraph:
-        "Legal residents physically located in any of the 50 states and Washington DC, excluding Alabama, Arizona, Hawaii, Idaho, Iowa, Louisiana, Montana, Nevada, and Washington are eligible to open an account and participate in both practice and contests offered by the SportsBattle app. Legal residents of Arizona, Hawaii, Iowa, Louisiana, Montana, Nevada, and Washington (the Excluded States) are ineligible for prizes offered by the app. Residents of the Excluded States are eligible to open and maintain accounts. You must be 18 years of age to play contests on SportsBattle & 21 if you are from Massachusetts ",
-    },
-  ]);
-  return (
-    <Box
-      sx={{
-        width: "100%",
-        minHeight: "100vh",
-        border: "1px solid #494949",
-        borderRadius: "4px",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <Typography
+
+  const [loading, setLoading] = useState(true);
+  const [faq, setFaq] = useState(null);
+
+  useEffect(() => {
+    if (openTag === "Frequently Asked Questions") {
+      setLoading(true);
+      getFaq()
+        .then((result) => {
+          setFaq(result);
+          setLoading(false);
+        })
+        .catch((error) => {
+          if (error) {
+            console.log(error);
+          }
+        });
+    }
+  }, [openTag]);
+
+  if (loading) {
+    return (
+      <Box
         sx={{
-          width: "90%",
-          color: "secondary.main",
-          fontSize: { lg: fs.normal, xs: fs.small, xxxs: fs.xs },
-          fontWeight: 600,
-          fontFamily: "poppins",
-          py: "13px",
+          width: "100%",
+          minHeight: "100vh",
+          border: "1px solid #494949",
+          borderRadius: "4px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        FAQ
-      </Typography>
-      <Box sx={{ width: "100%", background: "#494949", mb: "9px" }}>
+        <LoadingSpinnerEachSection />
+      </Box>
+    );
+  } else {
+    return (
+      <Box
+        sx={{
+          width: "100%",
+          height: "100vh",
+          maxHeight: "100vh",
+          border: "1px solid #494949",
+          borderRadius: "4px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          overflow: "scroll",
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
+        }}
+      >
         <Typography
           sx={{
             width: "90%",
-            color: "white",
-            fontSize: { lg: fs.small, xs: fs.xs, xxxs: fs.xxs },
+            color: "secondary.main",
+            fontSize: { lg: fs.normal, xs: fs.small, xxxs: fs.xs },
             fontWeight: 600,
             fontFamily: "poppins",
-            py: "9px",
-            margin: "0 auto",
+            py: "13px",
           }}
         >
-          About
+          FAQ
         </Typography>
+        {faq.map((each, index) => (
+          <Box
+            key={index}
+            sx={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Box sx={{ width: "100%", background: "#494949", mb: "9px" }}>
+              <Typography
+                sx={{
+                  width: "90%",
+                  color: "white",
+                  fontSize: { lg: fs.small, xs: fs.xs, xxxs: fs.xxs },
+                  fontWeight: 600,
+                  fontFamily: "poppins",
+                  py: "9px",
+                  margin: "0 auto",
+                }}
+              >
+                {each.title}
+              </Typography>
+            </Box>
+            {each.data.map((e, index) => (
+              <Box
+                key={index}
+                sx={{
+                  width: "90%",
+                  borderBottom: `${
+                    each.data.length === index + 1
+                      ? "none"
+                      : "1px solid #494949"
+                  }`,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                  mt: "12px",
+                }}
+              >
+                <Typography
+                  sx={{
+                    fontSize: { lg: fs.small, xs: fs.xs, xxxs: fs.xxs },
+                    fontWeight: 700,
+                    fontFamily: "poppins",
+                    color: "secondary.main",
+                    width: "100%",
+                    mb: "14px",
+                  }}
+                >
+                  {e.question}
+                </Typography>
+                <Typography
+                  sx={{
+                    fontSize: { lg: fs.small, xs: fs.xs, xxxs: fs.xxs },
+                    fontWeight: 500,
+                    fontFamily: "poppins",
+                    color: "secondary.main",
+                    width: "100%",
+                    mb: "10px",
+                  }}
+                >
+                  {e.answer}
+                </Typography>
+              </Box>
+            ))}
+          </Box>
+        ))}
       </Box>
-      {FAQnote.map((e, index) => (
-        <Box
-          key={index}
-          sx={{
-            width: "90%",
-            borderBottom: "1px solid #494949",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-start",
-            mt: "12px",
-          }}
-        >
-          <Typography
-            sx={{
-              fontSize: { lg: fs.small, xs: fs.xs, xxxs: fs.xxs },
-              fontWeight: 700,
-              fontFamily: "poppins",
-              color: "secondary.main",
-              width: "100%",
-              mb: "14px",
-            }}
-          >
-            {e.head}
-          </Typography>
-          <Typography
-            sx={{
-              fontSize: { lg: fs.small, xs: fs.xs, xxxs: fs.xxs },
-              fontWeight: 500,
-              fontFamily: "poppins",
-              color: "secondary.main",
-              width: "100%",
-              mb: "10px",
-            }}
-          >
-            {e.paragraph}
-          </Typography>
-        </Box>
-      ))}
-    </Box>
-  );
+    );
+  }
 }
 
 //get faqs
